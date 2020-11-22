7d2869289a3ce5d174b1d88a588f96f621f6d439
zamaraevk <zamaraevk@gmail.com>
2020-11-22 18:58:52 -0500

    Private company can send transaction at approval of every founder

--- contracts/PrivateCompany.sol
+++ contracts/PrivateCompany.sol
@@ -19,16 +19,32 @@ contract PrivateCompany is ERC20Private {
     uint256 internal constant TOKEN_VESTING_TIME = 4;
 
     /* Events */
+    event LogDeposit(address indexed sender, uint value);
     event LogFounderEquityDistribution(
-        address founderAddress,
+        address indexed founderAddress,
         uint256 equityAmount,
         uint256 vestingTime
     );
 
     event LogReleasedEquityForHolder(
-        address founderAddress,
+        address indexed founderAddress,
         uint256 equityAmount
     );
+    event LogTransactionSubmission(uint256 indexed transactionId);
+    event LogTransactionExecution(uint256 indexed transactionId);
+    event LogTransactionExecutionFailure(uint256 indexed transactionId);
+    event LogTransactionConfirmation(
+        address indexed sender,
+        uint256 indexed transactionId
+    );
+
+    /* Storage */
+    address[] public founders;
+    mapping(address => EquityHolder) equityHolders;
+    mapping(uint256 => Transaction) public transactions;
+    mapping(uint256 => mapping(address => bool)) public confirmations;
+
+    uint256 public transactionCount;
 
     /* Structs */
     struct EquityHolder {
@@ -38,16 +54,52 @@ contract PrivateCompany is ERC20Private {
         address holderAddress;
     }
 
-    /* Storage */
-    mapping(address => EquityHolder) equityHolders;
+    struct Transaction {
+        address destination;
+        uint256 value;
+        bytes data;
+        bool executed;
+    }
 
     /* Modifiers */
+    modifier notNull(address _address) {
+        require(_address != address(0));
+        _;
+    }
+
+    modifier transactionExists(uint256 transactionId) {
+        require(transactions[transactionId].destination != address(0));
+        _;
+    }
+
+    modifier confirmed(uint256 transactionId) {
+        require(confirmations[transactionId][msg.sender]);
+        _;
+    }
+
+    modifier notConfirmed(uint256 transactionId) {
+        require(!confirmations[transactionId][msg.sender]);
+        _;
+    }
+
+    modifier notExecuted(uint256 transactionId) {
+        require(!transactions[transactionId].executed);
+        _;
+    }
+
     modifier isFounder() {
         require(
             hasRole(OWNER_ROLE, msg.sender),
             "PrivateCompany: sender is not a founder"
         );
         _;
+    }
+
+    /**
+     * @dev Receive function allows to deposit ether.
+     */
+    receive() external payable {
+        if (msg.value > 0) LogDeposit(msg.sender, msg.value);
     }
 
     /* Public functions */
@@ -82,6 +134,7 @@ contract PrivateCompany is ERC20Private {
                 TOKEN_VESTING_TIME
             );
         }
+        founders = _founders;
     }
 
     /**
@@ -113,6 +166,76 @@ contract PrivateCompany is ERC20Private {
 
         _mint(msg.sender, unreleasedAmount);
         emit LogReleasedEquityForHolder(msg.sender, unreleasedAmount);
+    }
+
+    /**
+     * @dev Allows a founder to submit and confirm a transaction.
+     * @param destination Transaction target address.
+     * @param value Transaction ether value.
+     * @param data Transaction data payload.
+     */
+
+    function submitTransaction(
+        address destination,
+        uint256 value,
+        bytes memory data
+    ) public isFounder() returns (uint256 transactionId) {
+        transactionId = _addTransaction(destination, value, data);
+        confirmTransaction(transactionId);
+    }
+
+    /**
+     * @dev Allows a founder to confirm a transaction.
+     * @param transactionId Transaction ID.
+     */
+    function confirmTransaction(uint256 transactionId)
+        public
+        isFounder()
+        transactionExists(transactionId)
+        notConfirmed(transactionId)
+    {
+        confirmations[transactionId][msg.sender] = true;
+        emit LogTransactionConfirmation(msg.sender, transactionId);
+        executeTransaction(transactionId);
+    }
+
+    /**
+     * @dev Allows anyone to execute a confirmed transaction.
+     * @param transactionId Transaction ID.
+     */
+    function executeTransaction(uint256 transactionId)
+        public
+        isFounder()
+        confirmed(transactionId)
+        notExecuted(transactionId)
+    {
+        if (isConfirmed(transactionId)) {
+            Transaction storage txn = transactions[transactionId];
+            txn.executed = true;
+            if (_externalCall(txn.destination, txn.value, txn.data)) {
+                emit LogTransactionExecution(transactionId);
+            } else {
+                emit LogTransactionExecutionFailure(transactionId);
+                txn.executed = false;
+            }
+        }
+    }
+
+    /**
+     * @dev Returns the confirmation status of a transaction.
+     * @param transactionId Transaction ID.
+     * @return Confirmation status.
+     */
+    function isConfirmed(uint256 transactionId) public view returns (bool) {
+        uint256 count = 0;
+        for (uint256 i = 0; i < founders.length; i++) {
+            if (confirmations[transactionId][founders[i]]) {
+                count += 1;
+            }
+            if (count == founders.length) {
+                return true;
+            }
+        }
     }
 
     /* Private functions */
@@ -156,6 +279,53 @@ contract PrivateCompany is ERC20Private {
                     TOKEN_VESTING_TIME
                 );
         }
+    }
+
+    /* Internal functions */
+    /**
+     * @dev Adds a new transaction to the transaction mapping, if transaction does not exist yet.
+     * @param destination Transaction target address.
+     * @param value Transaction ether value.
+     * @param data Transaction data payload.
+     */
+    function _addTransaction(
+        address destination,
+        uint256 value,
+        bytes memory data
+    ) internal notNull(destination) returns (uint256 transactionId) {
+        transactionId = transactionCount;
+        transactions[transactionId] = Transaction({
+            destination: destination,
+            value: value,
+            data: data,
+            executed: false
+        });
+        transactionCount += 1;
+        emit LogTransactionSubmission(transactionId);
+    }
+
+    /**
+     * Call has been separated into its own function in order to take advantage
+     * of the Solidity's code generator to produce a loop that copies tx.data into memory.
+     */
+    function _externalCall(
+        address destination,
+        uint256 value,
+        bytes memory data
+    ) internal returns (bool) {
+        bool result;
+        assembly {
+            result := call(
+                gas(),
+                destination,
+                value,
+                add(data, 32), // First 32 bytes are the padded length of data, so exclude that
+                mload(data), // Size of the input (in bytes) - this is what fixes the padding problem
+                0,
+                0 // Output is ignored, therefore the output size is zero
+            )
+        }
+        return result;
     }
 }
 
